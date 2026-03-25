267bb935ca704667bb3ed2f5f86e59d3a1444a16
LuisP Cardenas <a01254955@tec.mx>
2026-03-25 08:24:40 -0700

    Feat: Fibonacci function

---
+++ lib.py
@@ -0,0 +1,7 @@
+def fibonacci(n):
+    secuencia = [0, 1]
+    while len(secuencia) < n:
+        siguiente = secuencia[-1] + secuencia[-2]
+        secuencia.append(siguiente)
+    return secuencia[:n]
+
